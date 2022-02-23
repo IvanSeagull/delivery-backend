6902ea9d2f67655a2424558bdbd0bb9c39dd6da4
IvanSeagull <47693465+IvanSeagull@users.noreply.github.com>
2022-02-23 17:34:52 +0000

    Starting project

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express")
+
+const app = express();
+
+const mainRouter = require("./Routes/mainRouter")
+
+app.use("/api", mainRouter)
+
+
+app.get("/", (req, res) => {
+	res.status(200).json({msg: "Home"})
+})
+const startApp = () => {
+	app.listen(5000, ()=> {
+	console.log("Listening server on port 5000")
+	})
+}
+
+
+
+startApp();
