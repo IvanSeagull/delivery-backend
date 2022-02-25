6e34a1350686b696d63da1231de0e1033382aae8
IvanSeagull <47693465+IvanSeagull@users.noreply.github.com>
2022-02-25 16:24:19 +0000

    Started working on register function

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,13 +1,42 @@
 const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const { SECRET } = require('../config');
 
 class userController {
   async getUsers(req, res) {
     res.status(200).json({ users: 'users' });
   }
   async register(req, res) {
-    let { username, email, password } = req.body;
-    console.log(username, email, password);
-    // res.status(200).json({ users: 'users' });
+    try {
+      let { username, email, password } = req.body;
+      console.log(username, email, password);
+
+      User.findAll({ where: { username: username } })
+        .then((users) => {
+          if (users.length > 0) {
+            return res.status(400).json({ msg: 'User with that username already exists' });
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+          return;
+        });
+
+      password = bcrypt.hashSync(password, 7);
+
+      User.create({
+        username,
+        email,
+        password,
+      })
+        .then((user) => {
+          console.log(user);
+          res.redirect('/');
+        })
+        .catch((err) => console.log(err));
+    } catch (error) {
+      return res.status(500).json({ error });
+    }
   }
   async login(req, res) {
     res.status(200).json({ msg: 'login' });
