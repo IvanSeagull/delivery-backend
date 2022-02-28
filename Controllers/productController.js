9836b5274b78afbe7bd2a95237c6a17df8ff1665
IvanSeagull <47693465+IvanSeagull@users.noreply.github.com>
2022-02-28 16:48:59 +0000

    ADded addProducts router

--- Controllers/productController.js
+++ Controllers/productController.js
@@ -9,6 +9,30 @@ class productController {
       return res.status(500).json({ error });
     }
   }
+  async addProducts(req, res) {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) return res.status(400).json({ errors: errors });
+
+      const { title, description, price, amount, discount } = req.body;
+      const newProduct = await Product.create({
+        title: title.toLowerCase(),
+        description,
+        price,
+        amount,
+        discount,
+      });
+      return res.status(200).json({ msg: 'Successfully added new category' });
+    } catch (error) {
+      return res.status(500).json({ error });
+    }
+  }
+  async updateProduct(req, res) {
+    console.log(123);
+  }
+  async deleteProduct(req, res) {
+    console.log(123);
+  }
 }
 
 module.exports = new productController();
--- Routes/productRouter.js
+++ Routes/productRouter.js
@@ -1,9 +1,24 @@
 const { Router } = require('express');
-const controller = require('../Controllers/productController');
+const { check } = require('express-validator');
 
+const controller = require('../Controllers/productController');
 const productRouter = new Router();
 
 productRouter.get('/', controller.getProducts);
+
+productRouter.get(
+  '/add',
+  [
+    check('title', 'Please enter title').notEmpty(),
+    check('description', 'Please enter description').notEmpty(),
+    check('price', 'Price should be number').notEmpty().isNumeric(),
+    check('amount', 'Amount should be number').notEmpty().isNumeric(),
+    check('discount', 'Discount should be number').notEmpty().isNumeric(),
+  ],
+  controller.addProducts,
+);
+productRouter.get('/update', controller.updateProduct);
+productRouter.get('/delete', controller.deleteProduct);
 
 // productRouter.get('/get-products', (req, res) => {
 //   res.status(200).json({ Product: 'Product' });
