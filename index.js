2696d98003f27666ba124d32cda777072e2609f9
IvanSeagull <47693465+IvanSeagull@users.noreply.github.com>
2022-03-02 14:24:57 +0000

    Started documenting routes with swagger(/api-docs)

--- index.js
+++ index.js
@@ -1,7 +1,47 @@
 const express = require('express');
+const app = express();
 const db = require('./db');
 
-const app = express();
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+// const swaggerOptions = {
+//   swaggerDefinition: {
+//     info: {
+//       title: 'Delivery backend',
+//       description: 'simple delivery backend',
+//       contact: {
+//         name: 'Ivan Seagull',
+//       },
+//       servers: ['http://localhost:5000'],
+//     },
+//   },
+//   apis: [
+//     // 'index.js',
+//     // '.Routes/userRouter.js',
+//     // '.Routes/categoryRouter.js',
+//     // '.Routes/productRouter.js',
+//   ],
+// };
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Delivery backend',
+      version: '1.0.0',
+      description: 'simple delivery backend',
+    },
+    servers: [
+      {
+        url: 'http://localhost:5000',
+      },
+    ],
+  },
+  apis: ['./Routes/*.js', 'index.js'],
+};
+
+const swaggerDocs = swaggerJsdoc(options);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use(express.json());
 
