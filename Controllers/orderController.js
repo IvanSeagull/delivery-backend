51101f183d71f6ff69218636c74ebcb420764783
IvanSeagull <47693465+IvanSeagull@users.noreply.github.com>
2022-03-04 13:32:29 +0000

    Worked on getting actual products items from orders instead of just ids

--- Controllers/orderController.js
+++ Controllers/orderController.js
@@ -1,4 +1,26 @@
 const Order = require('../models/Order');
+const Product = require('../models/Product');
+
+// get a list of actual products
+const getProductsData = async (order) => {
+  let productsId = [];
+  order.dataValues.orderProducts.forEach((el, index) => {
+    productsId.push(el.productId);
+  });
+
+  // find all products by id
+  let products = await Product.findAll({
+    attributes: ['id', 'title', 'description', 'price', 'amount', 'discount'],
+    where: { id: productsId },
+  });
+
+  // create new order object
+  order.dataValues.orderProducts = products.map((pr, index) => {
+    return { product: pr, amount: order.dataValues.orderProducts[index].amount };
+  });
+
+  return order;
+};
 
 class orderController {
   async getAllOrders(req, res) {
@@ -13,7 +35,28 @@ class orderController {
     try {
       let id = req.params.id;
       let order = await Order.findAll({ where: { id } });
-      res.status(200).json({ orders });
+      order = order[0];
+
+      let newOrder = await getProductsData(order);
+
+      res.status(200).json({ order: newOrder });
+    } catch (error) {
+      return res.status(500).json({ error });
+    }
+  }
+
+  async createOrder(req, res) {
+    try {
+      let { userId, orderProducts, totalPrice } = req.body;
+
+      const newOrder = await Order.create({
+        userId,
+        orderProducts,
+        totalPrice,
+        status: 'Processing',
+      });
+      //   return res.status(200).json({ msg: 'Successfully added new category' });
+      return res.status(200).json({ newOrder });
     } catch (error) {
       return res.status(500).json({ error });
     }
--- Routes/orderRouter.js
+++ Routes/orderRouter.js
@@ -9,6 +9,16 @@ orderRouter.get('/', controller.getAllOrders);
 
 orderRouter.get('/:id', controller.getOrder);
 
+orderRouter.post(
+  '/create',
+  // [
+  //   check('userId', 'Please enter a userId').isNumeric(),
+  //   check('orderProducts', 'Please enter a products').notEmpty(),
+  //   check('totalPrice', 'Please enter a price').isNumeric(),
+  // ],
+  controller.createOrder,
+);
+
 // orderRouter.get('/get-orders', (req, res) => {
 //   res.status(200).json({ orders: 'orders' });
 // });
